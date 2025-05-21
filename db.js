e97e64d042055733a09a900f194588ff4edcb3f1
Pranoy Sebastian <pranoysebastian@gmail.com>
2025-05-22 01:03:52 +0530

    removed unwanted code

--- db.js
+++ db.js
@@ -6,8 +6,6 @@ dotenv.config();
 const uri = process.env.MONGO_URI; // use dotenv for security
 console.log("Mongo URI:", process.env.MONGO_URI);
 const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
     ssl: true,
 });
 
