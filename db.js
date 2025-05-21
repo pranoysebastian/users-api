50407d1826807f5753fba45ed36d6b11e25b20c2
Pranoy Sebastian <pranoysebastian@gmail.com>
2025-05-22 00:59:27 +0530

    updated mongodb

--- db.js
+++ db.js
@@ -5,15 +5,19 @@ dotenv.config();
 
 const uri = process.env.MONGO_URI; // use dotenv for security
 console.log("Mongo URI:", process.env.MONGO_URI);
-const client = new MongoClient(uri);
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true,
+});
 
 export const connectToDB = async () => {
   try {
     await client.connect();
-    console.log("✅ Connected to MongoDB");
+    console.log("Connected to MongoDB");
     return client.db("myApp");
   } catch (err) {
-    console.error("❌ DB Connection Error:", err);
+    console.error("DB Connection Error:", err);
     process.exit(1);
   }
 };
